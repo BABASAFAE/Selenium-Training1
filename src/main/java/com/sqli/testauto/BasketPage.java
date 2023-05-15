51d0fcfde52c1211f77a576da63ae11b8f80cdd9
BABASAFAE <babasafae1@gmail.com>
2023-05-15 10:49:52 +0100

    Add two products using json delete two classes

--- src/main/java/com/sqli/testauto/BasketPage.java
+++ src/main/java/com/sqli/testauto/BasketPage.java
@@ -1,40 +1,27 @@
 package com.sqli.testauto;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
+import org.openqa.selenium.support.ui.FluentWait;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
+import java.util.NoSuchElementException;
 
 public class BasketPage {
 
     //check if the product is in the basket
-    public String procedToCheckout(WebDriver driver) throws InterruptedException {
-        driver.manage().deleteAllCookies();
-        Actions actions = new Actions(driver);
-        WebElement basket =driver.findElement(By.xpath("//button[@id=\"ta-mini-basket__open\"]"));//button[@id="ta-mini-basket__open"]
-
-        //Hovering on basket
-        actions.moveToElement(basket);
-
-
-        actions.click().build().perform();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        driver.manage().deleteAllCookies();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//tr[@class='MiniBasketItem']/td[@headers='capsules_original jamaica-blue-mountain-capsule-cafe-7726-50' and @class='MiniBasketItem__title']")));
-
-        WebElement  productInBasket =driver.findElement(By.xpath("//tr[@class='MiniBasketItem']/td[@headers='capsules_original jamaica-blue-mountain-capsule-cafe-7726-50' and @class='MiniBasketItem__title']"));
-
-
+    public String procedToCheckout(WebDriver driver,String productTitle) throws InterruptedException {
+        //Fluent wait setup
+        FluentWait waitfluentb = new FluentWait(driver)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofMillis(500))
+                .ignoring(NoSuchElementException.class);
+        //open basket
+        WebElement basket = (WebElement)  waitfluentb.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id=\"ta-mini-basket__open\"]")));
+        basket.click();
         // Move to product in basket and get title
-        actions.moveToElement(productInBasket);
+        WebElement  productInBasket=(WebElement)  waitfluentb.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(productTitle)));
         String titleProductInBasket= productInBasket.getText();
-
         return titleProductInBasket;
 
     }
